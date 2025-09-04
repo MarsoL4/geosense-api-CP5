0c8cccdafc421fc78df520de9608f33f16433311
MarsoL4 <enzogmarsola@gmail.com>
2025-09-04 09:41:39 -0300

    chore(application): fix namespaces and references in DashboardService

--- GeoSense.API/src/Application/Services/DashboardService.cs
+++ GeoSense.API/src/Application/Services/DashboardService.cs
@@ -1,46 +1,40 @@
-﻿namespace GeoSense.API.Services
+﻿using GeoSense.API.src.Application.DTOs;
+using GeoSense.API.src.Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeoSense.API.src.Application.Services
 {
-    using global::GeoSense.API.DTOs;
-    using global::GeoSense.API.Infrastructure.Contexts;
-    using Microsoft.EntityFrameworkCore;
-    using System;
-    using System.Linq;
-    using System.Threading.Tasks;
-
-    namespace GeoSense.Application.Services
+    public class DashboardService
     {
-        public class DashboardService
+        private readonly GeoSenseContext _context;
+
+        public DashboardService(GeoSenseContext context)
         {
-            private readonly GeoSenseContext _context;
+            _context = context;
+        }
 
-            public DashboardService(GeoSenseContext context)
+        public async Task<DashboardDTO> ObterDashboardAsync()
+        {
+            var hoje = DateTime.Today;
+
+            // Contar quantas alocações ocorreram hoje
+            var motosHoje = await _context.AlocacoesMoto
+                .Where(a => a.DataHoraAlocacao.Date == hoje)
+                .CountAsync();
+
+            // Calcular tempo de permanência em minutos
+            var tempos = await _context.AlocacoesMoto
+                .Select(a => (int)(DateTime.Now - a.DataHoraAlocacao).TotalMinutes)
+                .ToListAsync();
+
+            // Calcular média em horas, com arredondamento
+            double mediaHoras = tempos.Count > 0 ? tempos.Average() / 60.0 : 0;
+
+            return new DashboardDTO
             {
-                _context = context;
-            }
-
-            public async Task<DashboardDTO> ObterDashboardAsync()
-            {
-                var hoje = DateTime.Today;
-
-                // Contar quantas alocações ocorreram hoje
-                var motosHoje = await _context.AlocacoesMoto
-                    .Where(a => a.DataHoraAlocacao.Date == hoje)
-                    .CountAsync();
-
-                // Calcular tempo de permanência em minutos
-                var tempos = await _context.AlocacoesMoto
-                    .Select(a => (int)(DateTime.Now - a.DataHoraAlocacao).TotalMinutes)
-                    .ToListAsync();
-
-                // Calcular média em horas, com arredondamento
-                double mediaHoras = tempos.Count > 0 ? tempos.Average() / 60.0 : 0;
-
-                return new DashboardDTO
-                {
-                    TotalMotosHoje = motosHoje,
-                    TempoMedioPermanenciaHoras = Math.Round(mediaHoras, 2)
-                };
-            }
+                TotalMotosHoje = motosHoje,
+                TempoMedioPermanenciaHoras = Math.Round(mediaHoras, 2)
+            };
         }
     }
 }
