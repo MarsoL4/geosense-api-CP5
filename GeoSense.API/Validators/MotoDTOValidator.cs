b4fb2db00aa02798136ea9a5767f568b21a6c3f9
Enzo Marsola <enzogmarsola@gmail.com>
2025-10-20 19:39:55 -0300

    feat(validation): adicionar validators DTO com FluentValidation

---
+++ GeoSense.API/Validators/MotoDTOValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using GeoSense.API.DTOs.Moto;
+
+namespace GeoSense.API.Validators
+{
+    public class MotoDTOValidator : AbstractValidator<MotoDTO>
+    {
+        public MotoDTOValidator()
+        {
+            RuleFor(x => x.Modelo)
+                .NotEmpty().WithMessage("Modelo é obrigatório.")
+                .MaximumLength(50).WithMessage("Modelo deve ter no máximo 50 caracteres.");
+
+            RuleFor(x => x.Placa)
+                .NotEmpty().WithMessage("Placa é obrigatória.")
+                .Matches(@"^[A-Z0-9-]{1,10}$").WithMessage("Placa inválida. Use letras maiúsculas, números e '-' (até 10 chars).");
+
+            RuleFor(x => x.Chassi)
+                .NotEmpty().WithMessage("Chassi é obrigatório.")
+                .MaximumLength(50).WithMessage("Chassi deve ter no máximo 50 caracteres.");
+
+            RuleFor(x => x.VagaId)
+                .GreaterThan(0).WithMessage("VagaId deve ser maior que zero.");
+        }
+    }
+}
