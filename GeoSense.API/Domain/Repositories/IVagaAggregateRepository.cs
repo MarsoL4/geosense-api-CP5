bd2c640757c430410e72791ef279bf5ddaf65069
Enzo Marsola <enzogmarsola@gmail.com>
2025-10-20 20:18:58 -0300

    feat(domain): adicionar contrato IVagaAggregateRepository

---
+++ GeoSense.API/Domain/Repositories/IVagaAggregateRepository.cs
@@ -0,0 +1,19 @@
+﻿namespace GeoSense.API.Domain.Repositories
+{
+    using System.Threading.Tasks;
+    using System.Collections.Generic;
+    using GeoSense.API.Domain.Aggregates;
+
+    /// <summary>
+    /// Repositório de agregado para Vaga. Retorna/recebe o agregado (VagaAggregate)
+    /// e encapsula operações que refletem regras de negócio do agregado.
+    /// </summary>
+    public interface IVagaAggregateRepository
+    {
+        Task<List<VagaAggregate>> ObterTodasAsync();
+        Task<VagaAggregate?> ObterPorIdAsync(long id);
+        Task<VagaAggregate> AdicionarAsync(VagaAggregate vagaAggregate);
+        Task AtualizarAsync(VagaAggregate vagaAggregate);
+        Task RemoverAsync(VagaAggregate vagaAggregate);
+    }
+}
