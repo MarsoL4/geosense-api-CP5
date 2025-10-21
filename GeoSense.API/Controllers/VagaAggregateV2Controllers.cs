7a41590922d07bfb588ad9380ba95c66cd471f12
Enzo Marsola <enzogmarsola@gmail.com>
2025-10-20 22:03:14 -0300

    refactor(api): melhorar agrupamento Swagger do VagaAggregate v2 (tags)

--- GeoSense.API/Controllers/VagaAggregateV2Controllers.cs
+++ GeoSense.API/Controllers/VagaAggregateV2Controllers.cs
@@ -1,4 +1,5 @@
-﻿using GeoSense.API.Domain.Aggregates;
+﻿// Substitua o arquivo atual por este (mantém o mesmo comportamento, só adiciona Swagger tags)
+using GeoSense.API.Domain.Aggregates;
 using GeoSense.API.Domain.Repositories;
 using GeoSense.API.Helpers;
 using Microsoft.AspNetCore.Mvc;
@@ -21,9 +22,8 @@ namespace GeoSense.API.Controllers
         /// <summary>
         /// Aloca uma moto em uma vaga (regra do agregado).
         /// </summary>
-        /// <param name="id">Id da vaga</param>
-        /// <param name="body">Objeto com motoId</param>
         [HttpPost("{id:long}/alocar")]
+        [SwaggerOperation(Summary = "Aloca uma moto em uma vaga (agregado)", Tags = new[] { "VagaAggregate (v2)" })]
         [SwaggerResponse(204, "Moto alocada com sucesso (agregado)")]
         [SwaggerResponse(400, "Solicitação inválida ou regra de negócio violada")]
         [SwaggerResponse(404, "Vaga ou Moto não encontrado")]
@@ -52,6 +52,7 @@ namespace GeoSense.API.Controllers
         /// Libera a vaga (regra do agregado).
         /// </summary>
         [HttpPost("{id:long}/liberar")]
+        [SwaggerOperation(Summary = "Libera a vaga (agregado)", Tags = new[] { "VagaAggregate (v2)" })]
         [SwaggerResponse(204, "Vaga liberada com sucesso (agregado)")]
         [SwaggerResponse(404, "Vaga não encontrada")]
         public async Task<IActionResult> LiberarVaga(long id)
